49228321b3695570dfece1b9569e192b12232caa
Victor <pontarr@gmail.com>
2023-06-05 23:28:51 +0300

    Update jenkins.groovy

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -1,16 +1,55 @@
 pipeline {
     agent any
+    environment {
+        REPO = 'https://github.com/pontarr/mytelebot'
+        BRANCH = 'develop'
+        PATH = "/usr/local/go/bin:${env.PATH}"
+    }
     parameters {
 
-        choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
+        choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'darwin', 'all'], description: 'Pick OS')
+        
+        choice(name: 'ARCH', choices: ['amd64', 'arm64', '386', 'arm'], description: 'Pick ARCH')
 
     }
     stages {
-        stage('Example') {
+        stage("clone") {
             steps {
-                echo "Build for platform ${params.OS}"
+                echo 'Clone Repository'
+                git branch: "${BRANCH}", url: "${REPO}"
+                
+                  }
+         }
+        
+        stage("test") {
+            steps {
+                echo 'MAKE TEST'
+                sh 'make test'
+            }
+        }
 
-                echo "Build for arch: ${params.ARCH}"
+        stage("build") {
+            steps {
+                    echo 'MAKE BUILD:'
+                    sh "make build TARGETOS=${params.OS} TARGETARCH=${params.ARCH}"
+                  }
+        }
+
+        stage("image") {
+            steps {
+                    echo 'MAKE IAMGE:'
+                    sh "make image"
+                }
+            }
+
+        stage("push"){
+            steps {
+                echo 'MAKE PUSH'
+                script {
+                    docker.withRegistry('','ghcr.io') {
+                        sh 'make push'
+                    }
+                }
 
             }
         }
